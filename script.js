4b17244a64f2f112cf58b20d74acec9fdd60ae17
funkyghost617 <viplawnboy@gmail.com>
2025-11-17 13:03:41 -0600

    created navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `<header>database workshop</header>
+    <p>students</p>
+    <p>events</p>
+    <p>sub-events</p>
+    <p>registrations</p>`;
