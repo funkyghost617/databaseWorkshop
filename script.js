9988abc8c77dff67ea63c636550ffb427520580e
funkyghost617 <viplawnboy@gmail.com>
2025-11-17 14:56:29 -0600

    add links to navbar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navbar = document.getElementById("navbar");
 
-navbar.innerHTML = `<header>database workshop</header>
+navbar.innerHTML = `<header><a href="https://funkyghost617.github.io/databaseWorkshop">database workshop</a></header>
     <p><a href="/databaseWorkshop/pages/students.html">students</a></p>
-    <p>events</p>
-    <p>sub-events</p>
-    <p>registrations</p>`;
+    <p><a href="/databaseWorkshop/pages/events.html">events</a></p>
+    <p><a href="/databaseWorkshop/pages/sub-events.html">sub-events</a></p>
+    <p><a href="/databaseWorkshop/pages/registrations.html">registrations</a></p>`;
