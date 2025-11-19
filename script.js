a772b6f7aedc67fe8549a530d0722a0547f856eb
funkyghost617 <viplawnboy@gmail.com>
2025-11-19 15:36:56 -0600

    refactored repo folders

--- script.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-navbar.innerHTML = `<header><a href="https://funkyghost617.github.io/databaseWorkshop">database workshop</a></header>
-    <p><a href="/databaseWorkshop/pages/students.html">students</a></p>
-    <p><a href="/databaseWorkshop/pages/events.html">events</a></p>
-    <p><a href="/databaseWorkshop/pages/sub-events.html">sub-events</a></p>
-    <p><a href="/databaseWorkshop/pages/registrations.html">registrations</a></p>`;
---
+++ scripts/navbarScript.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `<header><a href="./students.html">database workshop</a></header>
+    <p><a href="./students.html">students</a></p>
+    <p><a href="./events.html">events</a></p>
+    <p><a href="./sub-events.html">sub-events</a></p>
+    <p><a href="./registrations.html">registrations</a></p>`;
