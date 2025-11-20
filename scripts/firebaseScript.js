48249774892161c895cb6aad26f3a42082279e6f
funkyghost617 <viplawnboy@gmail.com>
2025-11-19 21:56:41 -0600

    initialize firebase script connection

---
+++ scripts/firebaseScript.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAW8lBFXWUg7tfYbvod3-khX1oGXrnshKk",
+    authDomain: "databaseworkshop.firebaseapp.com",
+    projectId: "databaseworkshop",
+    storageBucket: "databaseworkshop.firebasestorage.app",
+    messagingSenderId: "115769503478",
+    appId: "1:115769503478:web:1e9c80f6a479035b3b86d7",
+    measurementId: "G-71JJBGLQXX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const messaging = getMessaging(app);
