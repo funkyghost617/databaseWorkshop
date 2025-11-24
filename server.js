f82eb60ef11dfd09835ad39ab1e64bf307339957
funkyghost617 <viplawnboy@gmail.com>
2025-11-24 11:32:28 -0600

    updated npm installation

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ import express from "express";
 import path from "path";
 const app = express();
 const port = process.env.PORT || 3000;
-const __dirname = ".";
 
 // serve static files from github pages project
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join("./", "public")));
 
 // define a basic route for the root
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join("./", "public", "index.html"));
 });
+
+app.get("/students/:id", async (req, res) => {
+    const studentID = req.params.id;
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
