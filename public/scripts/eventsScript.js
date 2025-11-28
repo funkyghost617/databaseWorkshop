f14ac40324eaf48038f53835139e1c2a627f6ea7
funkyghost617 <viplawnboy@gmail.com>
2025-11-28 13:42:51 -0600

    initialize eventsScript

--- public/scripts/eventsScript.js
+++ public/scripts/eventsScript.js
@@ -2,47 +2,47 @@
 import { collection, doc, getDoc, getDocs, addDoc, query, orderBy, where } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { db }  from "./firebaseScript.js";
 
-// create references to students table and table body
-const studentsTable = document.querySelector("#students-table");
-const studentsTableBody = document.querySelector("#students-table-body");
+// create references to events table and table body
+const eventsTable = document.querySelector("#events-table");
+const eventsTableBody = document.querySelector("#events-table-body");
 
-/* create array of strings corresponding to the property names of student documents in firebase
+/* create array of strings corresponding to the property names of event documents in firebase
     -- this array is used to create the table elements based on the query results submitted by the user
 */
-const studentAttributes = ["student_id", "first_name", "last_name", "primary_email", "secondary_email", "tertiary_email", "application_term", "date_of_birth", "address_line1", "address_line2", "address_city", "", "address_state", "address_country", "address_postcode"];
+const eventAttributes = ["event_name", "event_date", "event_time_start", "event_time_end", "event_location", "event_type"];
 
 // process each document which results from the firebase query
-function renderStudent(doc) {
-    let student = document.createElement("tr");
-    student.setAttribute("data-id", doc.id);
+function renderEvent(doc) {
+    let event = document.createElement("tr");
+    event.setAttribute("data-id", doc.id);
     let rowElements = [];
-    studentAttributes.forEach(element => {
+    eventAttributes.forEach(element => {
         rowElements.push(document.createElement("td"));
     })
     for (let i = 0; i < rowElements.length; i++) {
-        rowElements[i].textContent = doc.data()[studentAttributes[i]];
-        student.appendChild(rowElements[i]);
+        rowElements[i].textContent = doc.data()[eventAttributes[i]];
+        event.appendChild(rowElements[i]);
     };
-    studentsTableBody.appendChild(student);
+    eventsTableBody.appendChild(student);
 };
 
 // query full students table from firebase, then run renderStudent for each resulting document
-const studentsRef = collection(db, "students");
-const q = query(studentsRef, orderBy("student_id", "asc"));
+const eventsRef = collection(db, "events");
+const q = query(eventsRef, orderBy("event_date", "asc"));
 const querySnapshot = await getDocs(q);
-querySnapshot.forEach((student) => {
-    renderStudent(student);
+querySnapshot.forEach((event) => {
+    renderStudent(event);
 });
 
 // control display of current student modal
-const studentModal = document.querySelector("#student-modal");
-const closeStudentModal = document.querySelector("#close-modal-btn");
-closeStudentModal.addEventListener("click", () => {
-    studentModal.style.setProperty("display", "none");
+const eventModal = document.querySelector("#event-modal");
+const closeEventModal = document.querySelector("#close-modal-btn");
+closeEventModal.addEventListener("click", () => {
+    eventModal.style.setProperty("display", "none");
 });
 
 // create references to text elements within modal window
-const nameAndIDModal = document.querySelector("#name-and-id");
+/*const nameAndIDModal = document.querySelector("#name-and-id");
 const emailsModal = document.querySelector("#emails");
 const addressModal = document.querySelector("#address");
 const termAndDOBModal = document.querySelector("#term-and-dob");
@@ -69,35 +69,27 @@ DOB: ${row.children[7].textContent}`;
         recordLinkModal.setAttribute("href", "./students/" + docRef);
         studentModal.style.setProperty("display", "block");
     })
-});
+});*/
 
 // process raw csv text into new student documents
-const importCSVText = document.querySelector("#import-students-csv");
-const importCSVBtn = document.querySelector("#submit-import-students-csv");
+const importCSVText = document.querySelector("#import-events-csv");
+const importCSVBtn = document.querySelector("#submit-import-events-csv");
 const successMessage = document.querySelector("#success-message");
 importCSVBtn.addEventListener("click", (e) => {
     e.preventDefault();
     let importArray = importCSVText.value.split("\n");
-    importArray.forEach(student => {
-        let studentArray = student.split(",");
+    importArray.forEach(event => {
+        let eventArray = event.split(",");
         addDoc(collection(db, "students"), {
-            "student_id": Number(studentArray[0]),
-            "first_name": studentArray[1],
-            "last_name": studentArray[2],
-            "primary_email": studentArray[3],
-            "secondary_email": studentArray[4],
-            "tertiary_email": studentArray[5],
-            "application_term": studentArray[6],
-            "date_of_birth": studentArray[7],
-            "address_line1": studentArray[8],
-            "address_line2": studentArray[9],
-            "address_city": studentArray[10],
-            "address_state": studentArray[11],
-            "address_country": studentArray[12],
-            "address_postcode": studentArray[13]
+            "event_name": eventArray[0],
+            "event_date": eventArray[1],
+            "event_time_start": eventArray[2],
+            "event_time_end": eventArray[3],
+            "event_location": eventArray[4],
+            "event_type": eventArray[5],
         });
     });
-    successMessage.textContent = `${importArray.length} students added to database!`;
-    console.log(`${importArray.length} students added to database!`);
+    successMessage.textContent = `${importArray.length} events added to database!`;
+    console.log(`${importArray.length} events added to database!`);
     importCSVText.value = "";
 });
